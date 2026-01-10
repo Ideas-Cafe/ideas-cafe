9faa040c049369e29eb157bf65aac2a8fb749595
its-fz <fawwazajibola@gmail.com>
2026-01-10 14:27:45 +0100

    Changed screen blur conditions

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         }}
         id="Home"
       >
-        <div className="absolute inset-0 max-md:backdrop-blur-md max-md:bg-black/40" />
+        <div className="absolute inset-0 lg:backdrop-blur-md max-md:bg-black/20" />
         <div className="text-center space-y-2 text-amber-100 max-sm:-mt-8 animate-[slide-pop_2s_ease-in-out_1_alternate_forwards]">
           <p className="text-5xl font-bold mb-4 max-sm:mb-4.5 max-sm:text-[22.33px] max-sm:px-2 max-sm:font-bold max-sm:my-10">
             Ignite Your Passion. Shape <br className="max-sm:hidden" /> the
